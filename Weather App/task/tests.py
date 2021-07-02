da682271bda603e83b210898b90485d9929dcda7
jairo8925 <csarmiento2333@gmail.com>
2021-07-01 23:24:11 -0700

    allowed adding more cities

--- Weather App/task/tests.py
+++ Weather App/task/tests.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from hstest import FlaskTest, CheckResult, WrongAnswer
 from hstest import dynamic_test
@@ -39,12 +40,18 @@ class FlaskProjectTest(FlaskTest):
             raise WrongAnswer("Can't find a button with 'submit-button' class!")
         return button
 
+    def generate(self):
+        try:
+            if os.path.exists('web/weather.db'):
+                os.remove('web/weather.db')
+        except Exception as ignored:
+            raise WrongAnswer(f"Looks like your 'weather.db' database file is blocked. "
+                              f"Stop your apps that connects to that database!")
+        return []
+
     @classmethod
     async def check_cards_in_the_page(cls, page, cards_number):
         cards = await page.querySelectorAll('div.card')
-
-        if len(cards) == 0:
-            raise WrongAnswer("Can't find <div> blocks with class 'card'")
 
         if len(cards) != cards_number:
             raise WrongAnswer(f"Found {len(cards)} <div> blocks with class 'card', but should be {cards_number}!")
@@ -90,11 +97,18 @@ class FlaskProjectTest(FlaskTest):
             raise WrongAnswer("Can't find <div> block with class 'cards'")
 
         button = await self.get_submit_button(page)
+
+        if button is None:
+            raise WrongAnswer("Can't find a button with 'submit-button' class!")
+
         input_field = await self.get_input_field(page)
 
-        await self.check_cards_in_the_page(page, 3)
+        if input_field is None:
+            raise WrongAnswer("Can't find input field with 'input-city' id!")
 
-        await self.close_browser(browser)
+        await self.check_cards_in_the_page(page, 0)
+
+        await browser.close()
 
         return CheckResult.correct()
 
@@ -109,7 +123,7 @@ class FlaskProjectTest(FlaskTest):
         await page.goto(self.get_url())
 
         input_field = await self.get_input_field(page)
-        await input_field.type('Boston')
+        await input_field.type('London')
 
         button = await self.get_submit_button(page)
 
@@ -123,11 +137,70 @@ class FlaskProjectTest(FlaskTest):
         if cards_div is None:
             raise WrongAnswer("Can't find <div> block with class 'cards'")
 
-        await self.check_cards_in_the_page(page, 4)
+        await self.check_cards_in_the_page(page, 1)
 
     @dynamic_test(order=3)
     def test_add_city(self):
         asyncio.get_event_loop().run_until_complete(self.test_add_city_async())
+        return CheckResult.correct()
+
+    async def test_city_name_after_adding_async(self):
+
+        browser = await self.launch_and_get_browser()
+        page = await browser.newPage()
+        await page.goto(self.get_url())
+
+        input_field = await self.get_input_field(page)
+        await input_field.type('Fairbanks')
+
+        button = await self.get_submit_button(page)
+
+        await asyncio.gather(
+            page.waitForNavigation(),
+            button.click(),
+        )
+
+        cards_div = await page.querySelector('div.cards')
+
+        if cards_div is None:
+            raise WrongAnswer("Can't find <div> block with class 'cards'")
+
+        await self.check_cards_in_the_page(page, 2)
+
+    @dynamic_test(order=4)
+    def test_city_name_after_adding(self):
+        asyncio.get_event_loop().run_until_complete(self.test_city_name_after_adding_async())
+        return CheckResult.correct()
+
+    async def test_refresh_async(self):
+        browser = await self.launch_and_get_browser()
+        page = await browser.newPage()
+        await page.goto(self.get_url())
+
+        input_field = await self.get_input_field(page)
+        await input_field.type('Idaho')
+
+        button = await self.get_submit_button(page)
+
+        await asyncio.gather(
+            page.waitForNavigation(),
+            button.click(),
+        )
+
+        cards_div = await page.querySelector('div.cards')
+
+        if cards_div is None:
+            raise WrongAnswer("Can't find <div> block with class 'cards'")
+
+        await self.check_cards_in_the_page(page, 3)
+
+        await page.reload()
+
+        await self.check_cards_in_the_page(page, 3)
+
+    @dynamic_test(order=5)
+    def test_refresh(self):
+        asyncio.get_event_loop().run_until_complete(self.test_refresh_async())
         return CheckResult.correct()
 
 
--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -1,71 +1,82 @@
 import os
-
-from flask import Flask
-from flask import render_template
-from flask import request
-from datetime import datetime
-import requests
 import sys
+import requests
+from datetime import datetime
+from flask import Flask, abort, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+db = SQLAlchemy(app)
+url = 'https://api.openweathermap.org/data/2.5/weather'
 
 try:
     api_key = str(os.environ["API_KEY"])
 except KeyError:
-    print("Can't find api_key!")
-    sys.exit()
+    sys.exit("Can't find api_key!")
 
-url = 'https://api.openweathermap.org/data/2.5/weather'
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<City %r>' % self.name
+
+
+db.drop_all()
+db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    query = City.query.all()
+    cities_list = [x.name for x in query]
+    cities_info = []
+    for city_name in cities_list:
+        r = requests.get(url, params={'q': city_name, 'appid': api_key, 'units': 'metric'})
+
+        if r.status_code == 200:
+            data = r.json()
+        else:
+            return render_template('index.html')
+
+        city = data.get('name').upper()
+        degrees = round(int(data.get('main').get('temp')))
+        state = data.get('weather')[0].get('main')
+
+        offset = int(data.get('timezone'))
+        ts = int(data.get('dt')) + offset
+        local_hour = int(datetime.utcfromtimestamp(ts).strftime('%H'))
+
+        day_state = None
+        if 6 <= int(local_hour) <= 16:
+            day_state = 'day'
+        elif 17 <= int(local_hour) <= 23:
+            day_state = 'evening-morning'
+        elif 0 <= int(local_hour) <= 5:
+            day_state = 'night'
+
+        weather_info = {'city': city, 'degrees': degrees, 'state': state, 'day_state': day_state}
+
+        if all(weather_info.values()):
+            cities_info.append(weather_info)
+
+    return render_template('index.html', cities=cities_info)
 
 
 @app.route('/add', methods=['POST'])
 def add_city():
     city_name = request.form['city_name']
-    r = requests.get(url, params={'q': city_name, 'appid': api_key, 'units': 'metric'})
-
-    if r.status_code == 200:
-        data = r.json()
+    exists = db.session.query(City.id).filter_by(name=city_name).first() is not None
+    if exists:
+        print("City already exists")
     else:
-        print("Can't make connection to website. Try again.")
-        sys.exit()
-
-    # print("NAME:", weather_info.get('name'))
-    # print("TEMP:", weather_info.get('main').get('temp'))
-    # print("DESC:", weather_info.get('weather')[0].get('main'))
-
-    city = data.get('name').upper()
-    degrees = round(int(data.get('main').get('temp')))
-    state = data.get('weather')[0].get('main')
-
-    offset = int(data.get('timezone'))
-    ts = int(data.get('dt')) + offset
-    local_hour = int(datetime.utcfromtimestamp(ts).strftime('%H'))
-
-    day_state = None
-    if 6 <= int(local_hour) <= 16:
-        day_state = 'day'
-    elif 17 <= int(local_hour) <= 23:
-        day_state = 'evening-morning'
-    elif 0 <= int(local_hour) <= 5:
-        day_state = 'night'
-
-    dict_with_weather_info = {'city': city,
-                              'degrees': degrees,
-                              'state': state,
-                              'day_state': day_state}
-
-    if all(dict_with_weather_info.values()):
-        return render_template('add.html', weather=dict_with_weather_info)
-    else:
-        return render_template('index.html'), 404
+        db.session.add(City(name=city_name))
+        db.session.commit()
+    return redirect(url_for('index'))
 
 
-# don't change the following way to run flask:
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
